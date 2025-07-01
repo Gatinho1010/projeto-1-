30679783c20b39cb46c497c4c058e4f850b61b8e
Gatinho1010 <joao.fernando.dutra@escola.pr.gov.br>
2025-07-01 10:27:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Futebol',
+    'Idolo do flamengo',
+    'Arrascaeta'
+)
+
+criaCartao(
+    'Futebol',
+   'Quantos Libertadores o flamengo tem ?',
+    'O flamengo tem 3 '
+)
